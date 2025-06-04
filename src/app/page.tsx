024a4fd11c61b14ce2a61b52b8c4d0e6f48a77ac
belousmind <belousmind@gmail.com>
2025-06-04 18:25:14 +0300

    feat: add input component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Title from "@/components/title/title";
 import Button from "@/components/button";
 import TertiatyLink from "@/components/tertiaty-link";
 import Pagination from "@/components/pagination";
+import InputField from "@/components/fileds/input-field";
 
 export default function Page() {
   return (
@@ -12,6 +13,9 @@ export default function Page() {
       <Button text="button" variant="destroy" />
       <TertiatyLink href="pots" />
       <Pagination />
+      <InputField placeholder="search" title="titleeee" />
+      <InputField placeholder="search" withIcon={true} helpText="dfsdfsdfsd" />
+      <InputField placeholder="search" title="titleeee" withPrefix={true} type="number"/>
     </>
   );
 }
