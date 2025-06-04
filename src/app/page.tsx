880c15b0ef9b7f50b0c20b6918d0bf9fa8636e03
belousmind <belousmind@gmail.com>
2025-06-04 14:14:44 +0300

    feat: add TertiatyLink component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
 import Title from "@/components/title/title";
 import Button from "@/components/button";
-
+import TertiatyLink from "@/components/link";
 
 export default function Page() {
-  return  (
+  return (
     <>
-    <Title text="Overview" />
-    <Button text="button" variant="primary"/>
-    <Button text="button" variant="secondary"/>
-    <Button text="button" variant="destroy"/>
-
+      <Title text="Overview" />
+      <Button text="button" variant="primary" />
+      <Button text="button" variant="secondary" />
+      <Button text="button" variant="destroy" />
+      <TertiatyLink href="pots" />
     </>
-  ) 
+  );
 }
